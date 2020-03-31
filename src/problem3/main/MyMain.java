dae3b07b4bd49711a9aaa752eed19b9df39f5e45
Dikshaverma66 <45476205+Dikshaverma66@users.noreply.github.com>
2020-03-31 21:03:28 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,42 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
+import problem3.student.MyStudent;
+
+
 // executable class
-// use problem5.student.Student class to create object of student
+// use problem5.student.MyStudent class to create object of student
 public class MyMain {
+  
+  public static void main(String[] args) {
+
+        MyPriorityQueue queue = new MyPriorityQueue();
+
+        queue.enqueue(new MyStudent("Varsha",18));
+
+        queue.enqueue(new MyStudent("Diksha",13));
+
+        queue.enqueue(new MyStudent("Shreya",27));
+
+        queue.enqueue(new MyStudent("Ashish",58));
+
+        queue.enqueue(new MyStudent("Tavishi",10));
+
+        queue.enqueue(new MyStudent("Ananya",50));
+
+
+
+        queue.show();
+
+        queue.dequeue();
+
+        queue.show();
+
+
+
+    }
+
 }
